47e412581d366ed062c8821ea1f3eb01f87cf066
altari <sw.kim_altari@memebox.com>
2017-01-04 16:25:40 +0900

    gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 'use strict';
-
+var path = require('path');
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var watch = require('gulp-watch');
 var gutil = require('gulp-util');
-var browserSync_front = require('browser-sync').create('front');
-var nodemon = require('gulp-nodemon')
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+var Cache = require('gulp-file-cache');
 var del = require('del');
+var gulpWebpack = require('gulp-webpack');
+var webpack = require('webpack');
 var webpackConfig = require('./gulp/webpack.config');
-var webpackBuild = require('./gulp/webpack.build');
+
+var cache = new Cache();
 
 // config/////////////////////////
 var BASE_DIR = './';
@@ -16,14 +20,15 @@ var APP_DIR_RESOURCES = BASE_DIR + 'public/';
 var APP_DIR_APP = APP_DIR_RESOURCES + 'app/';
 var APP_DIR_DIST = APP_DIR_RESOURCES + 'dist/';
 var APP_DIR_VIEW = BASE_DIR + 'views/';
-var SERVER_DIR = BASE_DIR + 'controllers/';
+var SERVER_DIR = BASE_DIR + 'server/';
 
 var config = {
     BASE_DIR : BASE_DIR,
     APP_DIR_RESOURCES : APP_DIR_RESOURCES,
     APP_DIR_APP : APP_DIR_APP,
     APP_DIR_DIST : APP_DIR_DIST,
-    APP_DIR_VIEW : APP_DIR_VIEW
+    APP_DIR_VIEW : APP_DIR_VIEW,
+    APP_DIR_SERVER : SERVER_DIR
 };
 
 var files = {
@@ -34,48 +39,66 @@ var files = {
 };
 
 // task/////////////////////////
-gulp.task('nodemon', function (cb) {
-    var started = false;
+gulp.task('clean', function(){
+    return del.sync([config.APP_DIR_DIST]);
+});
 
+gulp.task('hbs', function() {
+    return gulp.src(files.view)
+});
+
+gulp.task('Webpack_mobile', function() {
+    return gulp.src(files.app)
+        // .pipe(cache.filter())
+        .pipe(gulpWebpack(webpackConfig('memebox', 'production', 'mobile')))
+        // .pipe(cache.cache())
+        .pipe(gulp.dest(config.APP_DIR_RESOURCES));
+});
+
+gulp.task('Webpack_pc', function() {
+    return gulp.src(files.app)
+        // .pipe(cache.filter())
+        .pipe(gulpWebpack(webpackConfig('memebox', 'production', 'pc')))
+        // .pipe(cache.cache())
+        .pipe(gulp.dest(config.APP_DIR_RESOURCES));
+});
+
+gulp.task('server', function() {
+    return gulp.src(files.server)
+});
+
+gulp.task('watch', function(){
+    var watcher = {
+        Webpack_pc: gulp.watch(files.app, ['Webpack_pc']),
+        Webpack_mobile: gulp.watch(files.app, ['Webpack_mobile']),
+        hbs: gulp.watch(files.view, ['hbs']),
+        server: gulp.watch(files.server, ['server'])
+    };
+
+    var notify = function(event) {
+        gutil.log('File', gutil.colors.yellow(event.path), 'was', gutil.colors.magenta(event.type));
+    };
+
+    for(var key in watcher) {
+        watcher[key].on('change', notify);
+    }
+});
+
+gulp.task('start', ['server'], function() {
     return nodemon({
-        script: 'server.js'
-    }).on('resatt', function () {
-        // to avoid nodemon being started multiple times
-        // thanks @matthisk
-        if (!started) {
-            cb();
-            started = true;
-        }
+        script: 'server.js',
+        watch: config.APP_DIR_SERVER
     });
 });
 
-gulp.task('local_build_pc', webpackBuild(webpackConfig('memebox', 'production', 'pc')));
-gulp.task('local_build_mobile', webpackBuild(webpackConfig('memebox', 'production', 'mobile')));
-
-gulp.task('front-browser-mobile-sync', ['local_build_mobile'], function() {//'local_build_pc',
-    browserSync_front.init({
+gulp.task('browser-sync', function(){
+    browserSync.init(null, {
         proxy: "http://localhost:5000",
-        browser: "google chrome",
+        files: [files.dist, files.view, files.server],
         port: 8000
-    });
-
-    gulp.watch(files.view).on("change", browserSync_front.reload);
-    gulp.watch(files.app).on("change", webpackBuild(webpackConfig('memebox', 'production', 'mobile')));
-    gulp.watch(files.dist).on("change", browserSync_front.reload);
+    })
 });
 
-gulp.task('front-browser-pc-sync', ['local_build_pc'], function() {//'local_build_pc',
-    browserSync_front.init({
-        proxy: "http://localhost:5000",
-        browser: "google chrome",
-        port: 8000
-    });
-
-    gulp.watch(files.view).on("change", browserSync_front.reload);
-    gulp.watch(files.app).on("change", webpackBuild(webpackConfig('memebox', 'production', 'pc')));
-    gulp.watch(files.dist).on("change", browserSync_front.reload);
-});
-
-gulp.task('default', ['nodemon'], function(){
-    return gutil.log('Gulp is running');
+gulp.task('default', ['clean', 'Webpack_mobile', 'Webpack_pc', 'watch', 'start', 'browser-sync'], function() {
+    gutil.log('Gulp is running');
 });
